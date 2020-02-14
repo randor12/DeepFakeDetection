b5ea7721e24005ea46181d94d2a00c044699a96c
randor12 <rynicholas@vt.edu>
2020-02-14 18:28:11 -0500

    Update TrainingModel.py

--- TrainingModel.py
+++ TrainingModel.py
@@ -4,14 +4,26 @@
 :description: This is the code for training the neural network
 """
 
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
+import numpy as np
+import os
 
-class TrainingModel:
+# Initialize the training model
+directory = os.getcwd()
+index = directory.index("DeepFakeDetection")
+slash = "/"
+if os.name == 'nt':
+    slash = "\\"
+directory = directory[0:index] + 'DeepFakeData' + slash
+train_file = directory + 'train_sample_videos'
 
-    def __init__(self, path):
-        """
-        Initialize the training model
-        :param path: path to data
-        """
-        self.path = path
+print("The training data is: ", train_file)
 
-    
+df = pd.read_json(os.path.join(train_file, 'metadata.json'))
+meta_train_df = df.T
+
+meta_train_df.head()
+
