b50584b2933d1a5b71544bf31eecc0c3255c0b16
randor12 <rynicholas@vt.edu>
2020-02-15 12:36:26 -0500

    Create ConvertingData.py

---
+++ ConvertingData.py
@@ -0,0 +1,39 @@
+"""
+:author: Ryan Nicholas
+:date: February 15, 2020
+:description: Convert data to be ready for training
+"""
+
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import to_categorical
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+
+
+class ConvertData():
+    def __init__(self, vals):
+        """
+        Initialize Data
+        :param df:
+        """
+        self.df = DataFrame(vals, columns=['index', 'label'])
+
+    def splitData(self):
+        """
+        Split the data for the file
+        :return:
+        """
+        # Get the x and y data
+        x = np.array(self.df.index.tolist())
+        y = np.array(self.df.label.tolist())
+
+        # Encode the classification labels
+        le = LabelEncoder()
+        yy = to_categorical(le.fit_transform(y))
+
+        # split the data
+        from sklearn.model_selection import train_test_split
+
+        x_train, x_test, y_train, y_test = train_test_split(x, yy, test_size=0.2, random_state=42, train_size=0.8)
+        return (x_train, x_test, y_train, y_test)
