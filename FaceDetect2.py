909105de7cfa2ba9886446bff42f1b5e32a5cd97
Matt Gonley <50349733+mattgonley@users.noreply.github.com>
2020-02-15 00:42:31 -0500

    update

--- FaceDetect2.py
+++ FaceDetect2.py
@@ -31,3 +31,29 @@ def detectDisplay
             eye_center = (x +x2 +w2//2, y+y2+h2//2)
             radius = int(round((w2+h2)*.25))
             frame = cv.circle(frame, eye_center, radius, (255,0,0),4)
+
+    cv.imshow('Capture', frame)
+
+parser =argparse.ArgumentParser(description='Cascade code')
+parser.add_argument('--face_cascade', help='Path tp face cascade', default='data/haarcascade_frontalface_alt.xml')
+parser.add_argument('--eye', help='Path to eye cascade', default='data/haarcascade_eye_tree_eyeglasses.xml')
+parser.add_argument('--camera', help='Camera divide number', type=int, defualt=0)
+args = parser.parse_args()
+
+face_cascade_name = args.face_cascade
+eyes_cascade_name = args.eyes_cascade
+
+face_cascade = cv.CascadeClassifier()
+eyes_cascade = cv.CascadeClassifier()
+
+if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
+    print('--(!)Error loading face image')
+    exit(0)
+if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
+    print('--(!)Error loading eye image')
+    exit(0)
+cam = args.camera
+cap = cv.VideoCapture(cam)
+if not cap.isOpened():
+    print('--(!)Error opening cam')
+    exit(0)
