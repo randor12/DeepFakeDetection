17143fbe0a8d2885cd5737dc85df5871ac0a29d5
diamond8658 <36394102+diamond8658@users.noreply.github.com>
2020-02-15 01:18:47 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -5,11 +5,24 @@
 """
 import tkinter as tk
 from tkinter import *
+from tkinter.ttk import *
 
 if __name__ == '__main__':
+    def open_file():
+        # Opens browser for files
+
+    def send():
+        # Sends file to AI
+
+    # Creates window
     window = tk.Tk()
     window.title("DeepFakeDetection")
+
+    # Creates items in window
     tk.Label(window, text = "Select File:").grid(row = 0, columnspan = 3)
     tk.Entry(window).grid(row = 1)
-    tk.Button(window, text = "Browse").grid(row = 1, column = 2)
+    tk.Button(window, text = "Browse", command = open_file).grid(row = 1, column = 2)
+    tk.Button(window, text = "Submit", command = send).grid(row = 2, column = 0, columnspan = 3)
+
+    progress = Progressbar()
     window.mainloop()
