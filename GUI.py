1baf0769440dcb8f36c35031e26b59879b010665
diamond8658 <36394102+diamond8658@users.noreply.github.com>
2020-02-15 02:12:57 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -11,18 +11,20 @@ if __name__ == '__main__':
     def open_file():
         i = 0
 
+
     def send():
         i = 0
+
 
     # Creates window
     window = tk.Tk()
     window.title("DeepFakeDetection")
 
     # Creates items in window
-    tk.Label(window, text = "Select File:").grid(row = 0, columnspan = 3)
-    tk.Entry(window).grid(row = 1)
-    tk.Button(window, text = "Browse", command = open_file).grid(row = 1, column = 2)
-    tk.Button(window, text = "Submit", command = send).grid(row = 2, column = 0, columnspan = 3)
+    tk.Label(window, text="Select File:").grid(row=0, columnspan=3)
+    tk.Entry(window).grid(row=1)
+    tk.Button(window, text="Browse", command=open_file).grid(row=1, column=2)
+    tk.Button(window, text="Submit", command=send).grid(row=2, column=0, columnspan=3)
 
     progress = Progressbar()
     window.mainloop()
