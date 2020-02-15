30b47fdcaa060f8ee9efda62645dddcab8f4e9aa
randor12 <rynicholas@vt.edu>
2020-02-15 01:33:51 -0500

    Update TrainingModel.py

--- TrainingModel.py
+++ TrainingModel.py
@@ -11,9 +11,9 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import os
 import cv2 as cv
-from skimage.transform import resize
-import keras
-from keras_preprocessing.image import ImageDataGenerator
+from sklearn import svm, metrics
+from IPython.display import Video
+plt.style.use('ggplot')
 
 
 def missing_data(data):
@@ -42,8 +42,6 @@ if os.name == 'nt':
 directory = directory[0:index] + 'DeepFakeData' + slash
 train_file = directory + 'train_sample_videos'
 
-print("The training data is: ", train_file)
-
 df = pd.read_json(os.path.join(train_file, 'metadata.json'))
 meta_train_df = df.T
 
@@ -61,33 +59,17 @@ for t_label in meta_train_df.label:
 train_label_extra = []
 
 # Get files
-train_data = np.array(list(train_file + meta_train_df.index))
-train_vids = []
-count = 0
-for x in train_data:
-    cap = cv.VideoCapture(x)
-    while cap.isOpened():
-        ret, frame = cap.read()
-
-        if not ret:
-            break
-        train_vids.append(frame)
-        train_label_extra.append(train_labels[count])
-    count += 1
-    cap.release()
-    cv.destroyAllWindows()
-
-storage = np.array([file for file in train_file if file.endswith('mp4')])
-
-#print(train_data)
-#print(storage.shape)
+train_data = np.array(list(train_file + slash + meta_train_df.index))
 
 # videos are 432 x 288 size
-
-x_train, x_test, y_train, y_test = train_test_split(train_vids, train_label_extra, test_size=0.2, random_state=42, train_size=0.8)
+x_train, x_test, y_train, y_test = train_test_split(train_file + slash + meta_train_df.index,
+                                                    meta_train_df.label,
+                                                    test_size=0.2,
+                                                    train_size=0.8,
+                                                    random_state=None)
 
 model = LinearRegression()
-model.fit(train_vids, train_label_extra)
+model.fit(x_train, y_train)
 """
 accuracy_on_train = model.score(x_train, y_train)
 accuracy_on_test = model.score(x_test, y_test)
