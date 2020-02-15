13e6e4ba767443148b41ff8d7f516e2a534b9209
Marcus Tran <46179752+Vysiar@users.noreply.github.com>
2020-02-15 15:55:17 -0500

    adding stuff to make gui look good
    image, button change

--- GUI.py
+++ GUI.py
@@ -4,29 +4,30 @@
 :description: This is our GUI code
 """
 import tkinter as tk
+from tkinter import filedialog
 from tkinter import *
 from tkinter.ttk import *
-from playsound import *
-
-if __name__ == '__main__':
-    def open_file():
-        i = 0 # temporary
 
 
-    def send():
-        i = 0 # temporary
-
-    def play_sound(string):
-        playsound(string)
+def UploadAction(event=None):
+    filename = filedialog.askopenfilename()
+    return filename
 
 
-    # Creates window
-    window = tk.Tk()
-    window.title("DeepVoice")
-    w, h = window.winfo_screenwidth(), window.winfo_screenheight()
-    window.geometry("%dx%d+0+0" % (w, h))
+# Creates window
+window = tk.Tk()
+window.title("DeepVoice")
+w, h = window.winfo_screenwidth(), window.winfo_screenheight()
+window.geometry("%dx%d+0+0" % (w, h))
 
+photo = PhotoImage(file = 'music_notes-512.png')
 
+uploadButton = tk.Button(window, text='Input', command=UploadAction, image=photo)
+uploadButton.config(height = 300, width = 300)
+uploadButton.grid(row=0, column=1)
+uploadButton.pack()
+
+window.mainloop()
 
     # Creates items in window
 
