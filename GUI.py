ee0d6d9ec4a5d965fa4f2e773c7a157005dae43b
Marcus Tran <46179752+Vysiar@users.noreply.github.com>
2020-02-15 15:17:16 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -4,7 +4,8 @@
 :description: This is our GUI code
 """
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import *
+from tkinter.ttk import *
 
 if __name__ == '__main__':
     def open_file():
@@ -15,18 +16,12 @@ if __name__ == '__main__':
         i = 0
 
 
-    def UploadAction(event=None):
-        filename = filedialog.askopenfilename()
-        print('Selected:', filename)
-
     # Creates window
     window = tk.Tk()
     window.title("DeepVoice")
     w, h = window.winfo_screenwidth(), window.winfo_screenheight()
     window.geometry("%dx%d+0+0" % (w, h))
-    uploadButton = tk.Button(window, text='Input', command=UploadAction)
-    uploadButton.pack()
+
 
     # Creates items in window
 
-    window.mainloop()
