799cc2d068f91aec9a51066debf24be52ea5c262
randor12 <rynicholas@vt.edu>
2020-04-07 22:03:35 -0400

    Initialized Training Video/Image Model File

---
+++ TrainVideo.py
@@ -0,0 +1,30 @@
+"""
+:author: Ryan Nicholas
+:date: April 7, 2020
+:description: Make the training model for image and video AI
+"""
+
+
+class TrainImage:
+    def __init__(self):
+        """
+        initialize image training classifier
+        """
+        self.X_Train = []
+        self.Y_Train = []
+        self.X_Test = []
+        self.Y_Test = []
+        self.X_Validate = []
+        self.Y_Validate = []
+
+    def train_image_classifier(self):
+        """
+        Train the image classifier
+        :return: None
+        """
+
+    def evaluate_model(self):
+        """
+        Evaluate model of the image classifier
+        :return: None
+        """
